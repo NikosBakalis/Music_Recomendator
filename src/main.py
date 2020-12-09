593763e030b1a47aba8ea2b7b3413129aaf7f83a
NikosBakalis <bakalis.nikolas@gmail.com>
2020-12-09 14:47:30 +0200

    Comments added.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 import pandas
+from sklearn.model_selection import train_test_split
 
+# pandas.set_option('display.max_columns', None)
+
+# Read the .csv file and turn it to a dataframe.
 df = pandas.read_csv("../data/spotify_dataset.csv", low_memory=False)
 
+# Take the columns of the dataframe we want as lists.
 userId = df[df.columns[0]].tolist()
 artistName = df[df.columns[1]].tolist()
 trackName = df[df.columns[2]].tolist()
 playlistName = df[df.columns[3]].tolist()
 
-df = pandas.DataFrame(list(zip(userId, artistName, trackName, playlistName)), columns=['User_Id', 'Artist_Name', 'Track_Name', 'Playlist_Name'])
+# And pass this columns to a new dataframe.
+df = pandas.DataFrame(list(zip(userId, artistName, trackName, playlistName)), columns=['User_Id', 'Artist_Name',
+                                                                                       'Track_Name', 'Playlist_Name'])
 
 print(df)
+
+train, test = train_test_split(df, test_size=0.5)
+print(train)
