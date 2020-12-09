20f680b02caf1ef60c7c1dd26efacc1a7374cc93
NikosBakalis <bakalis.nikolas@gmail.com>
2020-12-09 14:27:56 +0200

    Implementation of dataframe of our columns

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import pandas
 
 df = pandas.read_csv("../data/spotify_dataset.csv", low_memory=False)
 
-userId = df[df.columns[0]]
-artistName = df[df.columns[1]]
-trackName = df[df.columns[2]]
-playlistName = df[df.columns[3]]
+userId = df[df.columns[0]].tolist()
+artistName = df[df.columns[1]].tolist()
+trackName = df[df.columns[2]].tolist()
+playlistName = df[df.columns[3]].tolist()
 
-print(userId)
+df = pandas.DataFrame(list(zip(userId, artistName, trackName, playlistName)), columns=['User_Id', 'Artist_Name', 'Track_Name', 'Playlist_Name'])
+
+print(df)
