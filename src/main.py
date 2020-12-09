19c49c8920c43200202b2606ff3a9843d8b5fe57
NikosBakalis <bakalis.nikolas@gmail.com>
2020-12-09 02:28:28 +0200

    Columns added.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+import pandas
+
+df = pandas.read_csv("../data/spotify_dataset.csv", low_memory=False)
+
+userId = df[df.columns[0]]
+artistName = df[df.columns[1]]
+trackName = df[df.columns[2]]
+playlistName = df[df.columns[3]]
+
+print(userId)
+
 print("re")
