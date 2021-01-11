5e573c3305c6a5fb3fdf558b1c007a03e51e0575
DimitraKalam <demi.kalam@gmail.com>
2021-01-11 17:19:33 +0200

    data improvement

--- src/recomendator.py
+++ src/recomendator.py
@@ -1,9 +1,4 @@
-import functions
-from sklearn.model_selection import train_test_split
 import pandas as pd
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plots
 
 # content based recommender systems python
