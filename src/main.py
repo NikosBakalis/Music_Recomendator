b9e69b26fb1a8f1a55434f4b4bcad3feadbb0cbe
NikosBakalis <bakalis.nikolas@gmail.com>
2021-02-18 23:32:02 +0200

    Comments added.

--- src/main.py
+++ src/main.py
@@ -20,22 +20,29 @@ if not os.path.isfile("../data/correct_dataset.csv"):
 
 # region Loads and encodes the dataset.
 
+# Loads the dataset to X and y variables.
 X, y = functions.load_dataset("../data/correct_dataset.csv")
 
+# Prepares the inputs.
 X_enc, y_enc = functions.prepare_inputs(X, y)
 
 # endregion
 
 # region Splits the dataset to train and test.
 
+# Splits the dataset to train and test.
 X_train_enc, X_test_enc, y_train_enc, y_test_enc = train_test_split(X_enc, y_enc, train_size=0.75,  shuffle=True)
 
+# Combines the X_enc and the X.
 X_combine = numpy.stack((X_enc, X), axis=1)
 
+# Combines the y_enc and the y.
 y_combine = numpy.stack((y_enc, y), axis=1)
 
+# Ravels the y_train_enc.
 y_train_enc = y_train_enc.ravel()
 
+# Ravels the y_test_enc.
 y_test_enc = y_test_enc.ravel()
 
 # region Creation of dictionaries.
@@ -54,21 +61,29 @@ y_combine_dict = dict(zip(y_combine[::2], y_combine[1::2]))
 
 # region K-Nearest-Neighbors.
 
+# Initialize the classifier.
 knn = KNeighborsClassifier()
 
+# Fits the data.
 knn.fit(X_train_enc, y_train_enc)
 
+# The prediction.
 prediction = knn.predict(X_test_enc)
 
+# The result of the prediction
 result = 1 - spatial.distance.cosine(prediction, y_test_enc)
 
-print(y_test_enc[0], "\n")
+# print(y_test_enc[0], "\n")
 
-print((str(X_test_enc[0][0]), str(X_test_enc[0][1]), str(X_test_enc[0][2])))
+print(y_test_enc, "\n")
 
-print(X_combine_dict.get((str(X_test_enc[0][0]), str(X_test_enc[0][1]), str(X_test_enc[0][2]))), "\n")
+# print((str(X_test_enc[0][0]), str(X_test_enc[0][1]), str(X_test_enc[0][2])))
 
-print(prediction[0])
+# print(X_combine_dict.get((str(X_test_enc[0][0]), str(X_test_enc[0][1]), str(X_test_enc[0][2]))), "\n")
+
+# print(prediction[0])
+
+print(prediction)
 
 print(y_combine_dict.get(prediction[0]), "\n")
 
