655f23e914783d4de0be78d2654e4f61e621f50a
DimitraKalam <demi.kalam@gmail.com>
2021-02-24 18:25:15 +0200

    Plots final

--- src/plots.py
+++ src/plots.py
@@ -24,7 +24,6 @@ def plot(column_name):
     plt.subplots(figsize=(12,10))
     list1 = []
     for index, row in column_name.astype(str).items():
-    # for i in data['artistname']:
         list1.append(row)
     ax = pd.Series(list1).value_counts()[:10].sort_values(ascending=True).plot.barh(width=0.9,color=sns.color_palette('hls',10))
     for i, v in enumerate(pd.Series(list1).value_counts()[:10].sort_values(ascending=True).values):
